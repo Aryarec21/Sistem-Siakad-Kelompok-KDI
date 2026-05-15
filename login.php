40e7926f2398933890a7eee70e6231acea435133
SyafiqMuttaqin <89332514+Kkslofi@users.noreply.github.com>
2026-05-16 00:54:13 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include "config.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = md5($_POST['password']);
+
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+
+        $user = mysqli_fetch_assoc($result);
+
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['role'] = $user['role'];
+
+        header("Location: dashboard_mahasiswa.php");
+        exit;
+
+    } else {
+
+        echo "<script>
+            alert('Username atau Password salah!');
+            window.location.href='index.html';
+        </script>";
+    }
+
+} else {
+
+    header("Location: index.html");
+    exit;
+}
+?>
