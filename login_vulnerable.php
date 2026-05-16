abbe1f721c48eeec98f9868ba33b1d7b633ce441
SyafiqMuttaqin <89332514+Kkslofi@users.noreply.github.com>
2026-05-17 00:23:40 +0800

    Add files via upload

---
+++ login_vulnerable.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include "config.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+
+    $query = "SELECT * FROM users WHERE username = '$username'";
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = 'mahasiswa';
+
+        header("Location: dashboard_mahasiswa.php");
+        exit;
+    } else {
+        echo "Login gagal";
+    }
+} else {
+    header("Location: index.html");
+    exit;
+}
+?>
