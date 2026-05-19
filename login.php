281ec29201b862f36ece743eb5cfcf781bddd32c
Aryarec21 <aryarec63@gmail.com>
2026-05-19 16:45:25 +0700

    menyesuaikan sama databse yang baru

--- login.php
+++ login.php
@@ -5,16 +5,21 @@ include "config.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = md5($_POST['password']);
+    
+    // Menghapus md5() sementara karena password di database ('Tes123') berbentuk plaintext
+    // Jangan gunakan md5() jika di database belum menggunakan hash md5
+    $password = mysqli_real_escape_string($conn, $_POST['password']); 
 
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    // MENGUBAH 'users' menjadi 'user_auth'
+    $query = "SELECT * FROM user_auth WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn, $query);
 
     if (mysqli_num_rows($result) == 1) {
 
         $user = mysqli_fetch_assoc($result);
 
-        $_SESSION['id'] = $user['id'];
+        // MENGUBAH $user['id'] menjadi $user['id_user'] menyesuaikan kolom di database
+        $_SESSION['id'] = $user['id_user']; 
         $_SESSION['username'] = $user['username'];
         $_SESSION['role'] = $user['role'];
 
